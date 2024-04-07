3017358e712d062af2677732bf1163e0aa67a11b
Mlouise99 <mwubahalouise99@gmail.com>
2024-04-07 11:35:34 +0200

    Completed Add contact form

--- Contact-App/src/components/SignUpForm.jsx
+++ Contact-App/src/components/SignUpForm.jsx
@@ -1,18 +1,47 @@
-// SignUpForm.js
+
 import React, { useState } from 'react';
 
-const SignUpForm = ({ onSubmit }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const AddContact= ({ onSubmit }) => {
+const[fullname, setFullname]= useState('');
+const [email, setEmail] = useState('');
+const [phone, setPhone]= useState('');
+
+const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit({ email, password });
+    onSubmit({fullname,phone, email, password });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-5" htmlFor="name">
+         FullName
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="name"
+          placeholder="FullName"
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+          Phone
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="phone"
+          type="phone"
+          placeholder="Phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
           Email
         </label>
@@ -40,14 +69,14 @@ const SignUpForm = ({ onSubmit }) => {
       </div>
       <div className="flex items-center justify-between">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
-          Sign Up
+         Create
         </button>
       </div>
     </form>
   );
 };
 
-export default SignUpForm;
+export default AddContact;
--- Contact-App/src/pages/SignUpPage.jsx
+++ Contact-App/src/pages/SignUpPage.jsx
@@ -4,8 +4,8 @@ import SignUpForm from '../components/SignUpForm';
 
 function SignUpPage() {
   return (
-    <div className="container mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-5">Sign Up</h1>
+    <div className="container mx-auto mt-10  justify-center w-96 items-center border border-gray-100 rounded-lg p-4 bg-white  shadow-lg ">
+      <h1 className="text-2xl font-bold mb-5">ADD CONTACT FORM</h1>
       <SignUpForm />
     </div>
   );
